c9c071a0498f0da73325e96feaa9ed149cb06c95
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2023-12-18 15:48:11 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+const url = "https://swapi.dev/api/people";
+async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.results);
+}
 
 fetchData();
+
